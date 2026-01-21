07d0bf6508f3bbc84b9154b018720ce38569c16b
chenweilong <2502906272@qq.com>
2026-01-21 14:40:20 +0800

    - feat： 添加路由

---
+++ web/src/views/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Hello World</h1>
+    </div>
+    <HelloWorld msg="Vite + Vue + cwl" />
+</template>
+
+<script lang="ts" setup>
+import HelloWorld from '../components/HelloWorld.vue'
+</script>
